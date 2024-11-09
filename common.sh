7b599c645e3034b3a3de753d29310964986ffdce
Abdulqadirsidd <abdulqadirsidd2011@gmail.com>
2024-11-10 01:27:40 +0530

    Merge remote-tracking branch 'origin/main'

--- common.sh
+++ common.sh
@@ -4,25 +4,25 @@ log_file=/tmp/roboshop.log
 rm -f $log_file
 
 app_prerequisites() {
-  print_heading Add Application user"
+  print_heading "Add Application user"
   id roboshop &>>$log_file
   if [ $? -ne 0 ]; then
     useradd roboshop &>>$log_file
   f1
   status_check $?
 
-  print_heading Create Application Directory"
+  print_heading "Create Application Directory"
   rm -rf /app &>>$log_file
   mkdir /app  &>>$log_file
   status_check $?
 
-  print_heading Download Application Content"
+  print_heading "Download Application Content"
   curl -L -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip &>>$log_file
   status_check $?
 
   cd /app
 
-  print_heading Extract Application Content"
+  print_heading "Extract Application Content"
   unzip /tmp/$app_name.zip &>>$log_file
   echo $?
 }
