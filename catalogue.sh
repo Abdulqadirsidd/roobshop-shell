91300c020b990653883abd32fecdc616f06bd4a4
Abdulqadirsidd <abdulqadirsidd2011@gmail.com>
2024-11-12 04:01:42 +0530

    Merge remote-tracking branch 'origin/main'

--- catalogue.sh
+++ catalogue.sh
@@ -2,3 +2,20 @@ source common.sh
 app_name=catalogue
 
 nodejs_setup
+
+
+print_heading "Copy Mongodb repo file"
+cp mongodb.repo /etc/yum.repos.d/mongodb.repo &>>$log_file
+status_check $?
+
+print_heading "Install Mongodb Client"
+dnf install mongodb-mongosh -y &>>$log_file
+status_check $?
+
+print_heading "Load Master Data"
+mongosh --host mongodb.abdulqadir.shop </app/db/master-data.js
+status_check $?
+
+print_heading "Restart Catalogue Service"
+systemctl restart catalogue &>>$log_file
+status_check $?
