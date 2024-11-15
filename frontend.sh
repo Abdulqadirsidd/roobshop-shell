d766e68975d81591a18bc913961cd5039acd9434
Abdulqadirsidd <abdulqadirsidd2011@gmail.com>
2024-11-15 19:05:46 +0530

    Merge remote-tracking branch 'origin/main'

--- frontend.sh
+++ frontend.sh
@@ -32,8 +32,7 @@ print_heading "Extract Application content"
 unzip /tmp/frontend.zip &>>$log_file
 status_check $?
 
-# Install ansible
-# improvng
+
 print_heading "Start Nginx"
 systemctl enable nginx &>>$log_file
 systemctl restart nginx &>>$log_file
